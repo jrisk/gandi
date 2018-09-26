96aa949ac6e8514d0e704c86f202599f41832c96
Joey Risk <joeyrsk@gmail.com>
2018-09-26 10:51:18 -0400

    added back bootstrap css import to main vue

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 //import { createRouter } from './router/router.js'
 //import { store } from './store.js'
 
--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-//import 'bootstrap/dist/css/bootstrap.css'
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
 }
