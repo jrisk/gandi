b424a5e1b581beeaf6ecc44d45cdcad6d11d1cd0
Joey Risk <joeyrsk@gmail.com>
2018-12-06 00:36:33 -0500

    added mailgun,nodemailer,skoolia logo,mail reset ok

--- server.js
+++ server.js
@@ -123,6 +123,68 @@ var connection = mysql.createConnection({
 //USE CONNECTION POOLING INSTEAD
 connection.connect();
 
+
+//MAILGUN STUFF
+
+var mailgun = require('mailgun-js');
+var api_key = 'd462f4b5a10584cdddd1e6cfb9b486dd-52cbfb43-ee1e972c';
+var DOMAIN = 'jarisk.com';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
+
+var m_html = '<div style="padding: 5px; text-align: center; background-color: #06f;">';
+m_html += '<img src="http://www.jarisk.com/public/img/skoolia_logo.png" style="width: 163px; height: 32px;">';
+m_html += '</div>';
+m_html += '<div style="padding: 10px;">';
+m_html += 'Hi'; ////$this->get_dict(110, '', '', $lang;
+m_html += ',<br><br>';
+m_html += 'We\'ve received a request to reset your password. If you didn\'t make the request, just ignore this email. Otherwise, you can reset your password using this link';//$this->get_dict(111, '', '', $lang);
+m_html += '<br><br>';
+//reset link code
+m_html += '<a href="http://jarisk.com/reset/key" style="margin:0;font-family:"Helvetica Neue","Helvetica",Helvetica,Arial,sans-serif;border:1px solid;display:block;padding:10px 16px;text-decoration:none;border-radius:2px;text-align:center;vertical-align:middle;font-weight:bold;white-space:nowrap;background:white;border-color:#ff5a5f;background-color:#ff5a5f;color:white;border-top-width:1px;">';// . $this->get_dict(112, '', '', $lang) . '</a><br><br>';
+m_html += 'Click here to reset your password</a><br><br>';
+m_html += 'Regards';//$this->get_dict(113, '', '', $lang);
+m_html += ',<br><br>';
+m_html += 'The Skoolia Team';//$this->get_dict(114, '', '', $lang);
+
+var m_data = {
+  from: 'Skoolia <admin@jarisk.com>',
+  to: 'joeyrsk@gmail.com',
+  subject: 'Password Reset',
+  text: 'Testing out Mailgun',
+  html: m_html
+};
+
+var MailComposer = require('nodemailer/lib/mail-composer');
+
+var mail = new MailComposer(m_data);
+
+app.get('/testmailreset', function(req,res) {
+
+  mail.compile().build( function(err, msg) {
+
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    var data_send = {
+      to: 'joeyrsk@gmail.com',
+      message: msg.toString('ascii')
+    }
+
+    mailgun.messages().sendMime(data_send, function (error, body) {
+      if (error) {
+        console.log(error);
+        throw error;
+      }
+      console.log(body);
+    });
+
+  });
+
+  res.sendStatus(200);
+})
+
 app.use('/public', express.static(path.join(__dirname, './public')));
 
 app.use('/dist', express.static(path.join(__dirname, './dist')));
