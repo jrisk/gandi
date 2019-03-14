bc50c089feb23d0d724cd6378ad5ee11f2e82624
Joey Risk <joeyrsk@gmail.com>
2019-03-13 22:02:37 -0400

    main page update

--- src/components/jarisk/JariskMain.vue
+++ src/components/jarisk/JariskMain.vue
@@ -1,52 +1,23 @@
 <template>
   <div>
 <header>
-  <!--<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active" style="background-image: url('public/img/comp_outdoors.jpg')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">First Slide</h2>
-          <p class="lead">This is a description for the first slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item" style="background-image: url('https://4.bp.blogspot.com/-InDD3Hm_bhU/XB4_TK3TT7I/AAAAAAAAAJ4/r5tUeCOqq1MTchFh7D7pWdf582A4qYIIwCEwYBhgL/s1600/Businesswoman-working-at-a-computer-1280x720.jpg')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Second Slide</h2>
-          <p class="lead">This is a description for the second slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item" style="background-image: url('https://1.bp.blogspot.com/-GWl5F8P4t-8/XB4_VV4VE5I/AAAAAAAAAKE/SmHRzas-LpMpDBoLn6otR9AznVsr6L7OgCEwYBhgL/s1600/t1larg.africa.cnn.jpg')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Third Slide</h2>
-          <p class="lead">This is a description for the third slide.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-  </div>-->
   <b-carousel
     id="carousel-fade"
     style="text-shadow: 0px 0px 2px #000"
+    controls
     fade
     indicators
     img-width="1024"
     img-height="480"
-  >
-    <b-carousel-slide caption="Welcome" img-src="/public/img/comp_outdoors.jpg" />
-    <b-carousel-slide caption="Skoolia" img-src="/public/img/computer_job.png" />
-    <b-carousel-slide caption="Other" img-src="/public/img/online-learning.jpg" />
+    >
+    <a href="/home"><b-carousel-slide caption="Welcome" img-src="/public/img/comp_outdoors.jpg" />
+    </a>
+    
+    <a href="/skoolia"><b-carousel-slide caption="Skoolia" img-src="/public/img/woman_computer.jpg" />
+    </a>
+
+    <a href="/weather"><b-carousel-slide caption="Other" img-src="/public/img/people-using-computer.png" style="background-color: white" />
+    </a>
   </b-carousel>
 </header>
 
@@ -54,88 +25,64 @@
 <section class="py-5 text-center services-section">
       <div class="container"> 
         <h2 class="text-center">Jarisk Services</h2>
-        <p class="text-muted mb-5 text-center">Javascript</p>
+        <p class="text-muted mb-5 text-center">Web Development</p>
+
         <div class="row">
+
           <div class="col-sm-6 col-lg-4 mb-3">
-            <svg class="lnr text-primary services-icon">
-              <use xlink:href="#lnr-magic-wand"></use>
-            </svg>
-            <h6>Php</h6>
-            <p class="text-muted">Ex cupidatat eu officia consequat incididunt labore occaecat ut veniam labore et cillum id et.</p>
+            <h6>Javascript</h6>
+            <i class="fab fa-js-square"></i>
+            <p class="text-muted">Node, ECMA6, Angular, React, Vue</p>
           </div>
+
           <div class="col-sm-6 col-lg-4 mb-3">
-            <svg class="lnr text-primary services-icon">
-              <use xlink:href="#lnr-heart"></use>
-            </svg>
-            <h6>Web Dev</h6>
-            <p class="text-muted">Tempor aute occaecat pariatur esse aute amet.</p>
+            <h6>Full Stack</h6>
+            <i class="fas fa-ethernet"></i>
+            <p class="text-muted">HTML, CSS, Bootstrap, PHP, MySQL, Webpack</p>
           </div>
+
           <div class="col-sm-6 col-lg-4 mb-3">
-            <svg class="lnr text-primary services-icon">
-              <use xlink:href="#lnr-rocket"></use>
-            </svg>
             <h6>Web Admin</h6>
-            <p class="text-muted">Voluptate ex irure ipsum ipsum ullamco ipsum reprehenderit non ut mollit commodo.</p>
+            <i class="fas fa-tools"></i>
+            <p class="text-muted">5 Sites under Management</p>
+              <div class="d-flex">
+                <ul class="list-inline mx-auto justify-content-center">
+                  <li class="list-inline-item"><a class="main-links" href="https://topeurotips.com">Top Euro Tips</a></li> 
+                  <li class="list-inline-item"><a class="main-links" href="http://www.dothispodcast.com">Do This Podcast</a></li>
+                  <li class="list-inline-item"><a class="main-links" href="http://www.regionplural.com">Region Plural</a></li>
+                  <li class="list-inline-item"><a class="main-links" href="http://www.madcryptogains.com">Mad Crypto Gains (wip)</a></li>
+                </ul>
+              </div>
           </div>
+
           <div class="col-sm-6 col-lg-4 mb-3">
-            <svg class="lnr text-primary services-icon">
-              <use xlink:href="#lnr-paperclip"></use>
-            </svg>
             <h6>Writing</h6>
-            <p class="text-muted">Tempor commodo nostrud ex Lorem occaecat duis occaecat minim.</p>
+            <i class="fas fa-lightbulb"></i>
+            <p class="text-muted">Thoughts on electronic parchment</p>
           </div>
+
           <div class="col-sm-6 col-lg-4 mb-3">
-            <svg class="lnr text-primary services-icon">
-              <use xlink:href="#lnr-screen"></use>
-            </svg>
             <h6>Translations</h6>
-            <p class="text-muted">Et fugiat sint occaecat voluptate incididunt anim nostrud ea cillum cillum consequat.</p>
+            <i class="fas fa-comment"></i>
+            <p class="text-muted">Español, 日本語, or anything using translation APIs</p>
           </div>
+
           <div class="col-sm-6 col-lg-4 mb-3">
-            <svg class="lnr text-primary services-icon">
-              <use xlink:href="#lnr-inbox"></use>
-            </svg>
             <h6>Dev Ops</h6>
-            <p class="text-muted">Et labore tempor et adipisicing dolor.</p>
+            <i class="fas fa-cloud"></i>
+            <p class="text-muted">Stack-tracing, Log-reading, Server-starting</p>
           </div>
+
         </div>
       </div>
 </section>
 
   <section class="main">
     <div class="container mt-4">
-        <h1 class="text-center mb-4 p-4 blog-head">Writings</h1>
+        <h1 class="text-center mb-4 p-4 blog-head">Selected Quotes</h1>
         <div class="row blog-rows">
 
         <div class="card-columns">
-
-          <div class="card shadow border-0">
-            <img class="card-img-top" src="https://4.bp.blogspot.com/-InDD3Hm_bhU/XB4_TK3TT7I/AAAAAAAAAJ4/r5tUeCOqq1MTchFh7D7pWdf582A4qYIIwCEwYBhgL/s1600/Businesswoman-working-at-a-computer-1280x720.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title that wraps to a new line</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-          </div>
-
-          <div class="card shadow border-0 p-3">
-            <blockquote class="blockquote mb-0 card-body">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-              <footer class="blockquote-footer">
-                <small class="text-muted">
-                  Someone famous in <cite title="Source Title">Source Title</cite>
-                </small>
-              </footer>
-            </blockquote>
-          </div>
-
-          <div class="card shadow border-0">
-            <img class="card-img-top" src="https://4.bp.blogspot.com/-InDD3Hm_bhU/XB4_TK3TT7I/AAAAAAAAAJ4/r5tUeCOqq1MTchFh7D7pWdf582A4qYIIwCEwYBhgL/s1600/Businesswoman-working-at-a-computer-1280x720.jpg" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
 
           <div class="card shadow border-0 bg-primary text-white text-center p-3">
             <blockquote class="blockquote mb-0">
@@ -149,10 +96,32 @@
 
           <div class="card shadow border-0 p-3 text-right">
             <blockquote class="blockquote mb-0">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+              <p style="color: blue;">“It is important to reason from first principles then by analogy. The normal way we conduct our lives is we reason by analogy.  We are doing this because it's like something else that was done, or it is like what other people are doing. With first principles you boil things down to the most fundamental truths…and then reason up from there.”</p>
               <footer class="blockquote-footer">
                 <small class="text-muted">
-                  Someone famous in <cite title="Source Title">Source Title</cite>
+                  <cite title="Source Title">Elon Musk</cite>
+                </small>
+              </footer>
+            </blockquote>
+          </div>
+
+          <div class="card shadow border-0 p-3 bg-primary text-white text-right">
+            <blockquote class="blockquote mb-0">
+              <p>“We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology.”</p>
+              <footer class="blockquote-footer">
+                <small style="color: orange;">
+                  Carl Sagan
+                </small>
+              </footer>
+            </blockquote>
+          </div>
+
+          <div class="card shadow border-0 p-3 bg-primary text-white text-right">
+            <blockquote class="blockquote mb-0">
+              <p>“Everything that belongs to the body is a stream, and what belongs to the soul is a dream and vapor, and life is a warfare and a stranger's sojourn, and after fame is oblivion.”</p>
+              <footer class="blockquote-footer">
+                <small style="color: orange;">
+                  Marcus Aurelius
                 </small>
               </footer>
             </blockquote>
@@ -235,5 +204,13 @@ section {
 
 .blog-rows {
   color: orange;
+}
+
+.main-links {
+  text-decoration: none;
+}
+
+.main-list {
+  list-style: none;
 }
 </style>
